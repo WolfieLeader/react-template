8b43a3aa1863ae435b4b93c617f443cb3e370f8c
WolfieLeader <koren2318@gmail.com>
2023-08-12 01:47:16 +0300

    Color Palate

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,12 @@ export default {
   theme: {
     extend: {},
     colors: {
-      textColor: "rgb(var(--color-textColor))",
-      backgroundColor: "rgb(var(--color-backgroundColor))",
+      textColor: "rgb(var(--color-textColor) / <alpha-value>)",
+      backgroundColor: "rgb(var(--color-backgroundColor) / <alpha-value>)",
       primary: "rgb(var(--color-primary) / <alpha-value>)",
       secondary: "rgb(var(--color-secondary) / <alpha-value>)",
       accent: "rgb(var(--color-accent) / <alpha-value>)",
     },
   },
-  darkMode: "class",
   plugins: [],
 };
