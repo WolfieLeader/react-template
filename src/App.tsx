0ac371ff3ea2464369b078b77079666845c4faef
WolfieLeader <koren2318@gmail.com>
2023-08-11 12:52:22 +0300

    Adding Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from "react";
-import "./styles/index.css";
+import React, { lazy, Suspense } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./styles/default.css";
 
+import NavBar from "./components/NavBar";
+import Loading from "./pages/Loading";
+
+// * Import Here Any Page Component
+const Home = lazy(() => import("./pages/Home"));
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="flex flex-col h-screen">
+      <NavBar />
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
 
 export default App;
